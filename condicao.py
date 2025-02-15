c70ad4e583d4dd27c244ddf5fe95ea38e4b19d41
Leonardo Giampaglia Gomes <129779553+Giampaglia@users.noreply.github.com>
2025-02-15 10:22:45 -0300

    Create condicao.py

---
+++ condicao.py
@@ -0,0 +1,21 @@
+# IF e Else
+num1 = int(input("Digite um numero:\n"))
+print(type(num1))
+num2 = int(input("digite outro numero:\n"))
+print(type(num2))
+
+
+
+
+soma = num1+ num2
+
+# Verifica se o número é maior ou menor que 50
+
+if soma > 50:
+    print(f"o valor {soma} é maior que 50")
+elif soma == 50:
+    print(f"o valor {soma} é igual a 50")
+else:
+    print(f"o valor {soma} é menor que 50")
+
+    
